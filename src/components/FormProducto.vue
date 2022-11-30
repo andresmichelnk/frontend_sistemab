d420051a95959e3a88d759e96651cca706e0f075
mrtamayo <trx9607@gmail.com>
2022-11-30 11:53:05 -0500

    Modificando el FormProducto.vue

--- src/components/FormProducto.vue
+++ src/components/FormProducto.vue
@@ -3,19 +3,19 @@
     <label for="formGroupExampleInput" class="form-label separar-top separar-bottom">
       <h3>Nuevo Producto</h3>
     </label>
-    <input type="text" class="form-control" id="formGroupExampleInput" v-model="nombreProducto"
+    <input type="text" class="form-control" id="formGroupExampleInput1" v-model="nombreProducto"
       placeholder="Nombre del producto"><span class="alert" v-if="res1">{{ respuesta }}</span>
     <label for="formGroupExampleInput2" class="form-label"></label>
     <input type="text" class="form-control" id="formGroupExampleInput2" v-model="codigo"
       placeholder="Codigo del producto"><span class="alert" v-if="res2">{{ respuesta }}</span>
     <label for="formGroupExampleInput2" class="form-label"></label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" v-model="precio"
+    <input type="text" class="form-control" id="formGroupExampleInput3" v-model="precio"
       placeholder="Precio de Venta"><span class="alert" v-if="res3">{{ respuesta }}</span>
     <label for="formGroupExampleInput2" class="form-label"></label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" v-model="cantidad"
+    <input type="text" class="form-control" id="formGroupExampleInput4" v-model="cantidad"
       placeholder="Cantidad de producto"><span class="alert" v-if="res4">{{ respuesta }}</span>
     <label for="formGroupExampleInput2" class="form-label"></label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" v-model="medida"
+    <input type="text" class="form-control" id="formGroupExampleInput5" v-model="medida"
       placeholder="Unidad de medida del producto"><span class="alert" v-if="res5">{{ respuesta }}</span>
 
     <br>
