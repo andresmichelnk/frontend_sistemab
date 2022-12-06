462d154df736cea3b5cd1ee9279d926fedb8d911
andresmichelnk <michelandrestamayo@gmail.com>
2022-12-05 23:34:45 -0500

    Corrigiendo los Breakpoint - posicionamiento

--- src/pages/Producto.vue
+++ src/pages/Producto.vue
@@ -17,12 +17,12 @@ export default {
   <div class="container separar">
     <div class="container-fluid">
       <div class="row align-items-start ">
-        <div class="col-ms-12 col-md-5 col-lg-4">
-          <FormularioProducto />
-        </div>
         <div class="col-ms-12 col-md-7 col-lg-8">
           <h3 class="container-fluid  separar-top separar-bottom">Lista de Productos</h3>
           <ListaProductos class="separar-bottom" />
+        </div>
+        <div class="col-ms-12 col-md-5 col-lg-4">
+          <FormularioProducto />
         </div>
       </div>
     </div>
--- src/pages/Provedor.vue
+++ src/pages/Provedor.vue
@@ -17,12 +17,12 @@ export default {
   <div class="container separar">
     <div class="container-fluid">
       <div class="row align-items-start ">
-        <div class="col-ms-12 col-md-6 col-lg-4">
-          <FormularioProvedor />
-        </div>
         <div class="col-ms-12 col-md-6 col-lg-8">
           <h3 class="container-fluid separar-top separar-bottom">Listado de Provedores</h3>
           <ListaTrabajador class="separar-bottom" />
+        </div>
+        <div class="col-ms-12 col-md-6 col-lg-4">
+          <FormularioProvedor />
         </div>
       </div>
     </div>
